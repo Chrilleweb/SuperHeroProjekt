94217a4815eb222a042eda3e29045b6cb2c497d2
Chrilleweb <96743881+Chrilleweb@users.noreply.github.com>
2022-09-12 15:31:21 +0200

    indtast superhelt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Velkommen til programmet!");
+        System.out.println("-------------------------");
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Indtast superheltenavn");
+        String superHeltNavn = scan.next();
+        System.out.println("Indtast Superkraft");
+        String superKraft = scan.next();
+        System.out.println("Indtast superheltens rigtige navn");
+        String superHeltensRigtigeNavn = scan.next();
+        System.out.println("Indtast oprindelsesår");
+        int oprindelsesOr = scan.nextInt();
+        System.out.println("er din superhelt et menneske? (JA/NEJ)");
+        boolean erMennekse = scan.hasNext();
+        System.out.println("Indtast superheltens styrke");
+        double superhelteStyrke = scan.nextDouble();
+
+
     }
 }
