362af09e8f0f0b0a9eb14c35935b8a58b914aa1c
Chrilleweb <96743881+Chrilleweb@users.noreply.github.com>
2022-09-13 09:30:54 +0200

    superHero del 1..

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,23 +1,17 @@
 public class Database {
     private Superhero[] superheroes;
     private int antalSuperHeros;
-    private int superherosAmount;
 
 
     public Database() {
         superheroes = new Superhero[5];
         antalSuperHeros = 0;
-        superherosAmount = 0;
     }
     public void createSuperhero(String superHeltNavn, String superKraft, String superHeltensRigtigeNavn, int oprindelsesOr, String erMenneske, int superHelteStyrke){
         Superhero superhero = new Superhero(superHeltNavn, superKraft, superHeltensRigtigeNavn, oprindelsesOr, erMenneske, superHelteStyrke);
         superheroes[antalSuperHeros++] = superhero;
-        superheroes[antalSuperHeros++] = superhero;
         System.out.println(superhero);
-    }
-
-    public int getSuperherosAmount() {
-        return superherosAmount;
+        System.out.println(antalSuperHeros);
     }
 
     public int getAntalSuperHeros(){
