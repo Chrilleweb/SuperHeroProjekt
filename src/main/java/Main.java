ae589a12729abf06ce4ac23bb9e62aec77b97b1a
Chrilleweb <96743881+Chrilleweb@users.noreply.github.com>
2022-09-14 18:38:52 +0200

    pænere kode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,31 +2,19 @@ import java.util.Scanner;
 
 public class Main {
 
+    Scanner scan = new Scanner(System.in);
+    int opretEllerAfslut;
+    String superHeltNavn;
+    String superKraft;
+    String superHeltensRigtigeNavn;
+    int oprindelsesOr;
+    String erMenneske;
+    double superHelteStyrke;
+    String searchFor;
+    String searchForName;
     Database database = new Database();
 
-    public void indtastSuperHelt() {
-        System.out.println("Velkommen til programmet!");
-        System.out.println("-------------------------");
-        Scanner scan = new Scanner(System.in);
-        int opretEllerAfslut;
-        String superHeltNavn;
-        String superKraft;
-        String superHeltensRigtigeNavn;
-        int oprindelsesOr;
-        String erMenneske;
-        double superHelteStyrke;
-        String searchFor;
-        String searchForName;
-        do {
-
-        System.out.println("1. Opret superhelt");
-        System.out.println("2. Afslut");
-        System.out.println("3. Se liste af superhelte");
-        System.out.println("4. Søg efter superhelt");
-        opretEllerAfslut = scan.nextInt();
-        scan.nextLine();
-
-        if (opretEllerAfslut == 1){
+    public void OpretNySuperhelt(){
             System.out.println("Indtast superheltenavn");
             superHeltNavn = scan.nextLine();
             System.out.println("Indtast Superkraft");
@@ -43,34 +31,54 @@ public class Main {
             System.out.println("Indtast superheltens styrke");
             superHelteStyrke = scan.nextDouble();
             database.createSuperhero(superHeltNavn, superKraft, superHeltensRigtigeNavn, oprindelsesOr, erMenneske, superHelteStyrke);
-        }
 
-            if (opretEllerAfslut == 2) {
+    }
+
+    public void SearchForName(){
+            System.out.println("Hvilken superhelt vil du finde?");
+            searchForName = scan.nextLine();
+            System.out.println("Liste af superhelte der har " + searchForName + " i navnet:");
+            database.searchForName2(searchForName);
+            System.out.println();
+            System.out.println("Vælg en superhelt fra listen:");
+            searchFor = scan.nextLine();
+            System.out.println("Her er din superhelt:");
+            System.out.println(database.searchFor(searchFor));
+            System.out.println();
+
+    }
+
+    public void indtastSuperHelt() {
+        System.out.println("Velkommen til programmet!");
+        System.out.println("-------------------------");
+        do {
+
+        System.out.println("1. Opret superhelt");
+        System.out.println("2. Afslut");
+        System.out.println("3. Se liste af superhelte");
+        System.out.println("4. Søg efter superhelt");
+        opretEllerAfslut = scan.nextInt();
+        scan.nextLine();
+
+        if (opretEllerAfslut == 1){
+            OpretNySuperhelt();
+        } if (opretEllerAfslut == 2) {
                 System.out.println("Du har Afsluttet programmet");
                 System.exit(0);
             } if (opretEllerAfslut == 3){
                 System.out.println("Liste af superhelte:");
                  database.displaySuperheros();
             } if (opretEllerAfslut == 4){
-                System.out.println("Hvilken superhelt vil du finde?");
-                searchForName = scan.nextLine();
-                System.out.println("Liste af superhelte der har " + searchForName + " i navnet:");
-                database.searchForName2(searchForName);
-                System.out.println();
-                System.out.println("Vælg en superhelt fra listen:");
-                searchFor = scan.nextLine();
-                System.out.println("Her er din superhelt:");
-                System.out.println(database.searchFor(searchFor));
-                System.out.println();
+                SearchForName();
             }
-
-
 
         } while (opretEllerAfslut != 2);
     }
 
     public void start() {
         indtastSuperHelt();
+        OpretNySuperhelt();
+        SearchForName();
     }
 
     public static void main(String[] args) {
