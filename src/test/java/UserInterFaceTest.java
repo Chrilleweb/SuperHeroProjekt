f6537d91127c91f9f00ed78f2db4d17592942631
Chrilleweb <96743881+Chrilleweb@users.noreply.github.com>
2022-09-20 16:25:24 +0200

    test af userinterface

---
+++ src/test/java/UserInterFaceTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserInterFaceTest {
+
+    @Test
+    void deleteSuperhelt() {
+    }
+
+    @Test
+    void editSuperHeltNavn() {
+    }
+
+    @Test
+    void editSuperKraft() {
+    }
+
+    @Test
+    void editSuperHeltensRigtigeNavn() {
+    }
+
+    @Test
+    void editOprindelsesOr() {
+    }
+
+    @Test
+    void editErMenneske() {
+    }
+
+    @Test
+    void editSuperheltensStyrke() {
+    }
+
+    @Test
+    void editSuperhelt() {
+    }
+
+    @Test
+    void opretNySuperhelt() {
+    }
+
+    @Test
+    void searchForName() {
+    }
+
+    @Test
+    void indtastSuperHelt() {
+    }
+
+    @Test
+    void start() {
+    }
+}
